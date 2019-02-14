d6f065fa9e58ec023b151b3c5dbe807ce9f11b06
Clement Calmels <clement.calmels@free.fr>
2019-02-14 16:55:26 +0100

    emacs: use MELPA for addons

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,6 +4,8 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
 
 (set-language-environment "UTF-8")
