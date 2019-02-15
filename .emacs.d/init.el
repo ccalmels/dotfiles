8b52d0a76ddae426d36d1d033b7107b795ff3fad
Clement Calmels <clement.calmels@free.fr>
2019-02-15 16:12:25 +0100

    emacs: do not track custom.el anymore

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -86,7 +86,7 @@
 
 ;;; Customization
 (setq custom-file (concat user-emacs-directory "custom.el"))
-(load custom-file)
+(load custom-file 'noerror)
 
 ;;; Local Variables:
 ;;; after-save-hook: (lambda () (byte-compile-file buffer-file-name))
