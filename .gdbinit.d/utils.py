6aa82c9c87f22f9be5e99d2762e4dbef88c0e607
Clement Calmels <clement.calmels@free.fr>
2023-09-01 11:42:24 +0200

    gdb: improve stack command

--- .gdbinit.d/utils.py
+++ .gdbinit.d/utils.py
@@ -2,7 +2,10 @@ import gdb
 
 
 class UtilsStack(gdb.Command):
-    """ Show a stack like address """
+    """Show a stack like address:
+
+Usage: stack [EXP [LENGTH]]
+"""
 
     def __init__(self):
         super().__init__('stack', gdb.COMMAND_STACK)
@@ -15,11 +18,11 @@ class UtilsStack(gdb.Command):
         long_type_ptr = gdb.lookup_type('long').pointer()
         sp = sp.cast(long_type_ptr)
 
-        gdb.write(f'sp: {sp}\n')
+        gdb.write(f'sp: {sp.format_string(styling=True)}\n')
 
         for a in range(length - 1, -1, -1):
-            gdb.write(
-                f'sp+{4*a:3d}: {(sp + a).dereference().cast(long_type_ptr)}\n')
+            value = (sp + a).dereference().cast(long_type_ptr)
+            gdb.write(f'sp+{4*a:3d}: {value.format_string(styling=True)}\n')
 
 
 UtilsStack()
